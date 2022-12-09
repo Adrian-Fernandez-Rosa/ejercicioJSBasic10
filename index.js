501128a0b768d6db8d156205732986614ae66a4d
Adrián Fernández Rosa <adrianfr2011@gmail.com>
2022-12-09 15:20:51 -0300

    correcciones

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import {suma, multiplica} from './controller.js';
 import chalk from 'chalk';
 
 console.log(suma(2,4))
-console.log(chalk.green(multiplica(2,4)));
+console.log(chalk.green(multiplica(suma(1,2), suma(4, 5))));
